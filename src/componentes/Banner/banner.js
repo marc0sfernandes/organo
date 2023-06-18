c6f0685cf47377b87e8009227172250880bd2903
marc0sfernades <marcosvini1979.mv@gmail.com>
2023-06-18 11:09:43 -0300

    changes

---
+++ src/componentes/Banner/banner.js
@@ -0,0 +1,11 @@
+import "./banner.css"
+
+function Banner(){
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt= "banner principal da pagina do organo"></img>
+        </header>
+    )
+}
+
+export default Banner
