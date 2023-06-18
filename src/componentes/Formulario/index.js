6383ec710fb8eb16c70681db52eb88ab3f06b1ad
marc0sfernades <marcosvini1979.mv@gmail.com>
2023-06-18 12:36:54 -0300

    create component listaSuspensa

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,13 +1,21 @@
 import CampoTexto from '../CampoTexto/CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const Formulario = () => {
+    const times = [
+        'Top',
+        'Mid',
+        'Jungle',
+        'Adc',
+        'Suporte'
+    ]
     return (
         <section className='formulario'>
             <form>
                 <h2>Preencha os dados para criar o card do Player.</h2>
                 <CampoTexto label="Nome" placeholder="Digite o seu nome" />
-                <CampoTexto label="Função" placeholder="Digite a sua função" />
+                <ListaSuspensa label="Função" itens= {times}/>
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
             </form>
         </section>
