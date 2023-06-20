e214d8a0f7d7d485a7218f2ca5c4afa6bbebc1c4
marc0sfernades <marcosvini1979.mv@gmail.com>
2023-06-20 18:30:07 -0300

    create component colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css';
+
+const Colaborador = ()=>{
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/marc0sfernandes.png' alt='Marcos vinicius foto'></img>
+            </div>
+            <div className='rodape'>
+                <h4>Marcos Vinicius</h4>
+                <h5>Top</h5> 
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
